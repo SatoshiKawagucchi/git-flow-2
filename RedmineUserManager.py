2016ecc58a5e1cd66d4143880cf2b37934da6ed1
SatoshiKawagucchi <satoshi@kawagucchi.net>
2020-11-19 12:32:32 +0900

    Update RedmineUserManager.py

--- RedmineUserManager.py
+++ RedmineUserManager.py
@@ -668,16 +668,19 @@ class RedmineUserManager(BaseUserManager):
             print(f'VENDER_MAPオブジェクトが存在しません')
             raise NameError
             
-        # 非業遂判定
+        # 業遂／非業遂／Lychee利用判定
         try:
-            if not manager:
-                return f'{VENDER_MAP[company]}（非業遂）'
-            else:
-                # 業遂でLychee利用有無判定
+            if manager:
                 if not lycheeUser:
                     return f'{VENDER_MAP[company]}（業遂）（Lycheeなし）'
                 else:
                     return f'{VENDER_MAP[company]}（業遂）'
+            else:
+                # 業遂でLychee利用有無判定
+                if not lycheeUser:
+                    return f'{VENDER_MAP[company]}（非業遂）'
+                else:
+                    return f'{VENDER_MAP[company]}（非業遂）（Lycheeあり）'
         except Exception as e:
             print(f'Error: 指定したベンダーコードは存在しません {e}')
             return False
